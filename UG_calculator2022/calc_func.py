4de1e0bb68821e6b383657334e3526453761fcdb
Ugoo <ugookoma(at)icloud.com>
2022-07-04 09:34:02 +0200

    Initial commit

---
+++ UG_calculator2022/calc_func.py
@@ -0,0 +1,23 @@
+#Add functionalities
+
+#For addition
+from tracemalloc import stop
+
+
+def add(first_num, second_num):
+    return (first_num + second_num)
+
+#For subtraction
+def subtract(first_num, second_num):
+    return (first_num - second_num)
+
+#For multiplication
+def multiplication(first_num, second_num):
+    return (first_num * second_num)
+
+#For Division
+def division(first_num, second_num):
+    return (first_num / second_num)
+
+
+#The end
